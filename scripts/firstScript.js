67283a8f424e7d69b7808c9517ee29ed05c4cd8a
Camille Gautier <camille.gaut@sfr.fr>
2022-11-22 18:05:03 +0100

    first commit

---
+++ scripts/firstScript.js
@@ -0,0 +1,10 @@
+let varTest = "Coucou la console";
+
+console.log(varTest);
+console.log("Salut la console");
+
+
+let firstElement = document.createElement("p");
+firstElement.textContent = "oibevponôznônze";
+
+document.body.appendChild(firstElement);
